6edea92eb16372eb0fd0a1c72e781fa06345b79e
manicdon7 <manicdon7@gmail.com>
2024-01-28 09:10:08 +0530

    sem2

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-// App.js
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from './src/screens/HomeScreen';
-import GPAComponent from './src/screens/GPAComponent';
+import Sem1 from './src/screens/Sem1';
+import Sem2 from './src/screens/Sem2';
 
 const Stack = createStackNavigator();
 
@@ -12,8 +12,9 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name=" " component={HomeScreen} />
-        <Stack.Screen name="Details" component={GPAComponent} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Semester 1" component={Sem1} />
+        <Stack.Screen name="Semester 2" component={Sem2} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,46 @@
 // src/screens/HomeScreen.js
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
+import { styled } from 'nativewind';
+import Sem1 from './Sem1'; // Import Sem1 component
+import Sem2 from './Sem2'; // Import Sem2 component
+
+const StyledView = styled(View);
+const StyledText = styled(Text);
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <StyledView>
+      <StyledText className='text-center my-5'>Calculate Your CGPA</StyledText>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#3498db',
+          padding: 15,
+          borderRadius: 25,
+          alignItems: 'center',
+          margin: 10,
+        }}
+        onPress={() => navigation.navigate('Semester 1')} // Navigate to Sem1 when button is clicked
+      >
+        <StyledText style={{ color: 'white', fontSize: 18 }}>
+          Semester 1
+        </StyledText>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{
+          backgroundColor: '#3498db',
+          padding: 15,
+          borderRadius: 25,
+          alignItems: 'center',
+          margin: 10,
+        }}
+        onPress={() => navigation.navigate('Semester 2')} // Navigate to Sem2 when button is clicked
+      >
+        <StyledText style={{ color: 'white', fontSize: 18 }}>
+          Semester 2
+        </StyledText>
+      </TouchableOpacity>
+    </StyledView>
   );
 };
 
